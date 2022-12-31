2e3ed535ebb4bd92fb4d1b66212ebd2fe2c4488c
Glitch (tf-virtual-pet) <none>
2022-12-31 02:50:16 +0000

    👮🐁 Checkpoint ./public/client.js:52695792/3794

--- public/client.js
+++ public/client.js
@@ -1,49 +1,64 @@
-$(function() {
- 
+$(function () {
   // Add a variable "pet_info" equal to a object with the name (string), weight (number), and happiness (number) of your pet
+
+  let pet_info = {
+    name: "Thinkpup",
+    weight: 6,
+    happiness: 0,
+  };
 
   // Called function to update the name, happiness, and weight of our pet in our HTML
   checkAndUpdatePetInfoInHtml();
 
   // When each button is clicked, it will "call" function for that button (functions are below)
-  $('.treat-button').click(clickedTreatButton);
-  $('.play-button').click(clickedPlayButton);
-  $('.exercise-button').click(clickedExerciseButton);
-  
-
-
+  $(".treat-button").click(clickedTreatButton);
+  $(".play-button").click(clickedPlayButton);
+  $(".exercise-button").click(clickedExerciseButton);
 
   function clickedTreatButton() {
-      // Increase pet happiness
+    // Increase pet happiness
+    pet_info["happiness"] = pet_info["happiness"] + 1;
+
     // Increase pet weight
+    pet_info["weight"] = pet_info["weight"] + 2;
+
     checkAndUpdatePetInfoInHtml();
   }
-  
+
   function clickedPlayButton() {
     // Increase pet happiness
+    pet_info['happiness'] = pet_info['happiness'] + 2;
+    
     // Decrease pet weight
     checkAndUpdatePetInfoInHtml();
+    pet_info['weight'] = pet_info['weight'] - 1;
   }
-  
+
   function clickedExerciseButton() {
     // Decrease pet happiness
+    pet_info['happiness'] = pet_info['happiness'] - 1;
+
     // Decrease pet weight
+    pet_info['weight'] = pet_info['weight'] - 1;
     checkAndUpdatePetInfoInHtml();
   }
 
   function checkAndUpdatePetInfoInHtml() {
-    checkWeightAndHappinessBeforeUpdating();  
+    checkWeightAndHappinessBeforeUpdating();
     updatePetInfoInHtml();
   }
-  
+
   function checkWeightAndHappinessBeforeUpdating() {
     // Add conditional so if weight is lower than zero, set it back to zero
+    if (pet_info['weight'] < 1) {
+      pet_info['weight'] = 1;
+    }
   }
-  
+
   // Updates your HTML with the current values in your pet_info object
   function updatePetInfoInHtml() {
-    $('.name').text(pet_info.name);
-    $('.weight').text(pet_info.weight);
-    $('.happiness').text(pet_info.happiness);
+    $(".name").text(pet_info.name);
+    $(".weight").text(pet_info.weight);
+    $(".happiness").text(pet_info.happiness);
   }
-})
+});
